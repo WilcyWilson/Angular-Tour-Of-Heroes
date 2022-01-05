45fe2ae336c7bf0c1f713d0928913c0a697ee486
WilcyWilson <wilsonshresthaapps@gmail.com>
2022-01-05 17:51:29 +0545

    Feature components, class binding

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { Hero } from './hero';
-import { HEROES } from './mock-heroes';
+import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -8,14 +8,18 @@ import { HEROES } from './mock-heroes';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  heroes = HEROES;
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
-  constructor() { }
+  heroes : Hero[] = [];
+  selectedHero?: Hero;
+  constructor(private heroService: HeroService) { }
 
   ngOnInit(): void {
+    // this.getHeroes();
   }
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+
+
 
 }
